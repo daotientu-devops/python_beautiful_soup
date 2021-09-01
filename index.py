fbd65c1c94b625a0e33d2aac8838580273f19b3f
tudao <tudao@5nhatnhat.com>
2021-09-01 23:22:23 +0700

    - [Beautiful Soup]: + Find elements by ID + Find elements by HTML class name and text content + Extract text from HTML elements + Access parent elements + Extract attributes from HTML elements

--- index.py
+++ index.py
@@ -1,8 +1,30 @@
 from bs4 import BeautifulSoup
 import requests
-url = "https://www.tutorialspoint.com/index.htm"
+url = "https://realpython.github.io/fake-jobs/"
 req = requests.get(url)
 soup = BeautifulSoup(req.content, "html.parser")
-print(soup.title)
-for link in soup.find_all('a'):
-    print(link.get('href'))
+results = soup.find(id="ResultsContainer")
+print(results.prettify())
+job_elements = results.find_all("div", class_="card-content")
+for job_element in job_elements:
+    title_element = job_element.find("h2", class_="title")
+    company_element = job_element.find("h3", class_="company")
+    location_element = job_element.find("p", class_="location")
+    print(title_element.text.strip())
+    print(company_element.text.strip())
+    print(location_element.text.strip())
+python_jobs = results.find_all("h2", class_="title")
+print(python_jobs)
+# You're passing an anonymous function to the string argument. The lambda function looks at the text of each <h2> element,
+# converts it to lowercase and checks whether the sybstring "python" is found anywhere
+python_jobs = results.find_all("h2", string=lambda text: "python" in text.lower())
+print(len(python_jobs))
+python_job_elements = [
+    h2_element.parent.parent.parent for h2_element in python_jobs
+]
+for job_element in python_job_elements:
+    links = job_element.find_all('a')
+    for link in links:
+        # Using ["href"] to extract
+        link_url = link["href"]
+        print(f"Apply here: {link_url}\n")
